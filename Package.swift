5786896e0cc3c48059fca2a59f257e636721e01a
= <niccolo.dellarocca@gmail.com>
2024-11-29 13:53:41 +0100

    Initialised package

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -11,11 +9,27 @@ let package = Package(
             name: "ZTronCarousel",
             targets: ["ZTronCarousel"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", branch: "develop"),
+        .package(url: "https://github.com/NiccoloBargioni/ZTronObservation", branch: "bugfix/threadSafety"),
+        .package(url: "https://github.com/NiccoloBargioni/ZTronCarouselCore", branch: "main"),
+        .package(url: "https://github.com/NiccoloBargioni/ZTronSerializable", branch: "main"),
+        .package(url: "https://github.com/Juanpe/SkeletonView", branch: "main")
+    ],
+
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZTronCarousel"),
+            name: "ZTronCarousel",
+            resources: [
+                .process("Resources")
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency=complete")
+            ]
+
+        ),
         .testTarget(
             name: "ZTronCarouselTests",
             dependencies: ["ZTronCarousel"]
