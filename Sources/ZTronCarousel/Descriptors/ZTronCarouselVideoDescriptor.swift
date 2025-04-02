268fb2355eb398d925a76373aa320a97c5c4e973
= <niccolo.dellarocca@gmail.com>
2025-04-02 18:56:21 +0200

    Further effort to fully support videos

--- Sources/ZTronCarousel/Descriptors/ZTronCarouselVideoDescriptor.swift
+++ Sources/ZTronCarousel/Descriptors/ZTronCarouselVideoDescriptor.swift
@@ -1,11 +1,7 @@
 import ZTronDataModel
 import ZTronCarouselCore
 
-public final class ZTronCarouselVideoDescriptor: ZTronVisualMediaDescriptor {
-    private(set) public var type: ZTronCarouselCore.VisualMedia = .video
-
-    private let assetName: String
-    private let `extension`: String
+public final class ZTronCarouselVideoDescriptor: ZTronVideoDescriptor, ZTronVisualMediaDescriptor {
     private let caption: String
     
     public init(
@@ -13,19 +9,10 @@ public final class ZTronCarouselVideoDescriptor: ZTronVisualMediaDescriptor {
         extension: String,
         caption: String
     ) {
-        self.assetName = assetName
-        self.extension = `extension`
         self.caption = caption
+        super.init(assetName: assetName, withExtension: `extension`)
     }
-    
-    public func getAssetName() -> String {
-        return self.assetName
-    }
-    
-    public func getExtension() -> String {
-        return self.extension
-    }
-    
+        
     public func getCaption() -> String {
         return self.caption
     }
